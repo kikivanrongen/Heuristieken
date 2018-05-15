f68cccff848661d0085e2e0fec9c27f36f52e312
ZooeyBossert <zooey.bossert@gmail.com>
2018-05-15 13:51:54 +0200

    Dijkstra added

---
+++ algorithms/Dijkstra.py
@@ -0,0 +1,41 @@
+import csv
+import classes.classes
+from functions.random_trajectory_noreturns import random_trajectory_noreturns
+import random
+
+def dijkstra(data):
+
+    min = 0
+    max_min = 120
+    train = classes.classes.Train(start, data)
+    start = random.choice(data.names)
+    previous =
+
+    # while loop for constrains
+    while min < max_min:
+    
+        # possible connections from last station
+        possible = data.connections[train.location]
+
+        # make sure train does not go to previous station
+        if previous in possible:
+            possible.remove(previous)
+
+        # make dict of time and connection
+        my_dict = {item : possible[index+1] for index, item in enumerate(a) if index % 2 == 0}
+        my_dict = {}
+        for index, item in enumerate(a):
+            if index % 2 == 0:
+                my_dict[item] = a[index+1]
+
+        print(my_dict)
+        # pick shortest connection from possible connections
+        next =
+
+        train.update_trajectory(next)
+        min = train.time_elapsed
+
+        # update previous location
+        previous = train.location
+
+    return train
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import csv
+import random
 import classes.classes
 from algorithms.firstsol import firstsol
 from algorithms.firstsol_noreturn import firstsol_noreturn
@@ -20,25 +21,35 @@ NZ_Holland.stations("data/StationsHolland.csv")
 NZ_Holland.railroads("data/ConnectiesHolland.csv")
 
 # calculate score for random and cornetstart
-random = []
-corner = []
-for i in range(10):
-    random.append(firstsol(NZ_Holland).score())
-    corner.append(cornerstart(NZ_Holland).score())
-    random_noreturn.append(firstsol_noreturn(NZ_Holland).score())
-    corner_noreturn.append(cornerstart_noreturn(NZ_Holland).score())
+# random = []
+# corner = []
+# for i in range(10):
+#     random.append(firstsol(NZ_Holland).score())
+#     corner.append(cornerstart(NZ_Holland).score())
+#     random_noreturn.append(firstsol_noreturn(NZ_Holland).score())
+#     corner_noreturn.append(cornerstart_noreturn(NZ_Holland).score())
+#
+# mean_random = sum(random)/len(random)
+# mean_corner = sum(corner)/len(corner)
+# mean_random_noreturn = sum(random_noreturn)/len(random_noreturn)
+# mean_corner_noreturn = sum(corner_noreturn)/len(corner_noreturn)
+#
+#
+# print("FIRSTSOL:")
+# print(mean_random)
+# print("UITHOEKSOL:")
+# print(mean_corner)
+# print("FIRSTSOL NO RETURN:")
+# print(mean_random_noreturn)
+# print("UITHOEKSOL NO RETURN:")
+# print(mean_corner_noreturn)
+start = random.choice(NZ_Holland.names)
+train = classes.classes.Train(start, NZ_Holland)
+possible = NZ_Holland.connections[train.location]
+#my_dict = {item : possible[index+1] for index, item in enumerate(possible) if index % 2 == 0}
+my_dict = {}
+for index, item in enumerate(possible):
+    if index % 2 == 0:
+        my_dict[item] = possible[index+1]
 
-mean_random = sum(random)/len(random)
-mean_corner = sum(corner)/len(corner)
-mean_random_noreturn = sum(random_noreturn)/len(random_noreturn)
-mean_corner_noreturn = sum(corner_noreturn)/len(corner_noreturn)
-
-
-print("FIRSTSOL:")
-print(mean_random)
-print("UITHOEKSOL:")
-print(mean_corner)
-print("FIRSTSOL NO RETURN:")
-print(mean_random_noreturn)
-print("UITHOEKSOL NO RETURN:")
-print(mean_corner_noreturn)
+print(my_dict)

5182c72da1a7e75ce2f30be3a7c0486594088eea
ZooeyBossert <zooey.bossert@gmail.com>
2018-05-22 15:30:16 +0200

    dijkstra added no return path

--- algorithms/Dijkstra.py
+++ algorithms/Dijkstra.py
@@ -8,26 +8,53 @@ def dijkstra(data):
 
     min = 0
     max_min = 120
-    start = random.choice(data.names)
+
+    # start and end critical stations but randomly chosen
+    start = random.choice(data.critical_stations)
+    end = random.choice(data.critical_stations)
+    while start == end:
+        end = random.choice(data.critical_stations)
+
     train = classes.classes.Train(start, data)
+    previous = []
+    previous.append(start)
+
+    print("!!!!!!!!BEGIN!!!!!!!!!!")
 
     # while loop for constrains
     while min < max_min:
-
+        # mag geen previous hebben , check voor eind station anders door naar korte verbinding
         # possible connections from last station
         possible = data.connection_and_time[train.location]
 
-        # sort the tuple list
+        #print(possible)
+        print(previous)
+        print("\n")
+        print(possible)
+        print("\n")
+
+        # make sure train does not go to previous station
+        for prev in previous:
+            for pos in possible:
+                    if prev[0] == pos[0]:
+                            possible.remove(prev)
+
+        print(possible)
+        print("--------")
+
+        # sort the tuple list and pick station with shortest time
         time = sorted(possible, key=lambda times: times[1])
         shortest_time = time[0]
-        print(shortest_time)
-        # pick shortest connection from possible connections
-        next =
+        next = shortest_time[0]
+
+        # if next station is end station return train
+        if next == end:
+            return train.past_stations
 
         train.update_trajectory(next)
         min = train.time_elapsed
 
         # update previous location
-        previous = train.location
+        previous.append(train.location)
 
-    return train
+    return train.past_stations

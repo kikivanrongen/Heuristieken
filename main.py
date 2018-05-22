5f171937cade118b6607ef4ace7e918eda4ec2db
Kiki van Rongen <Kiki@MacBook-Air-van-Kiki.local>
2018-05-22 15:25:05 +0200

    added argument to algorithm functions

--- main.py
+++ main.py
@@ -9,11 +9,13 @@ from algorithms.cornerstart_noreturn import cornerstart_noreturn
 from algorithms.hillclimber import hillclimber
 from algorithms.greedy import greedy
 
+# print("Which area would you like to ride? (Nederland / NZHolland)")
+
 NZ_Holland = classes.classes.Stations()
 NZ_Holland.stations("data/StationsHolland.csv")
 NZ_Holland.railroads("data/ConnectiesHolland.csv")
 
-# calculate score for random and cornetstart
+# calculate score for random and cornerstart
 random = []
 corner = []
 random_noreturn = []
@@ -21,13 +23,13 @@ corner_noreturn = []
 hill_climber = []
 greedy_alg = []
 
-for i in range(100):
+for i in range(10000):
     random.append(firstsol(NZ_Holland).score())
     corner.append(cornerstart(NZ_Holland).score())
     random_noreturn.append(firstsol_noreturn(NZ_Holland).score())
     corner_noreturn.append(cornerstart_noreturn(NZ_Holland).score())
-    hill_climber.append(hillclimber(NZ_Holland).score())
-    greedy_alg.append(greedy(NZ_Holland).score())
+    # hill_climber.append(hillclimber(NZ_Holland).score())
+    # greedy_alg.append(greedy(NZ_Holland).score())
 
 mean_random = sum(random)/len(random)
 mean_corner = sum(corner)/len(corner)
@@ -35,9 +37,6 @@ mean_random_noreturn = sum(random_noreturn)/len(random_noreturn)
 mean_corner_noreturn = sum(corner_noreturn)/len(corner_noreturn)
 mean_hillclimber = sum(hill_climber)/len(hill_climber)
 mean_greedy = sum(greedy_alg)/len(greedy_alg)
-
-lijst = [mean_random, mean_random_noreturn, mean_corner, mean_corner_noreturn]
-print(lijst)
 
 
 print("FIRSTSOL:")

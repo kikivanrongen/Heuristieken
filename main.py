63d43af058d9b656f3084882fa9bb85f97db3777
Eleanoor Polder <Eleanoor@wcw-stud-145-109-24-130.wireless.uva.nl>
2018-05-22 15:28:02 +0200

    visualisatie en verandering main

--- main.py
+++ main.py
@@ -8,10 +8,24 @@ from algorithms.cornerstart import cornerstart
 from algorithms.cornerstart_noreturn import cornerstart_noreturn
 from algorithms.hillclimber import hillclimber
 from algorithms.greedy import greedy
+from functions.random_trajectory_noreturns import random_trajectory_noreturns
 
+from visualisation.visual import visual_solution
+from visualisation.visual import visual
+
+# load data NZ_Holland
 NZ_Holland = classes.classes.Stations()
 NZ_Holland.stations("data/StationsHolland.csv")
 NZ_Holland.railroads("data/ConnectiesHolland.csv")
+
+# load data the Nederlands
+Nederland = classes.classes.Stations()
+Nederland.stations("data/StationsNationaal.csv")
+Nederland.railroads("data/ConnectiesNationaal.csv")
+
+# visualisation of possible trajectory
+visual(NZ_Holland, "Visualisatie NZ Holland")
+visual(Nederland, "Visualisatie Nederland")
 
 # calculate score for random and cornetstart
 random = []
@@ -21,7 +35,7 @@ corner_noreturn = []
 hill_climber = []
 greedy_alg = []
 
-for i in range(100):
+for i in range(3):
     random.append(firstsol(NZ_Holland).score())
     corner.append(cornerstart(NZ_Holland).score())
     random_noreturn.append(firstsol_noreturn(NZ_Holland).score())
@@ -42,13 +56,71 @@ print(lijst)
 
 print("FIRSTSOL:")
 print(mean_random)
+print(max(random))
 print("UITHOEKSOL:")
 print(mean_corner)
+print(max(corner))
 print("FIRSTSOL NO RETURN:")
 print(mean_random_noreturn)
+print(max(random_noreturn))
 print("UITHOEKSOL NO RETURN:")
 print(mean_corner_noreturn)
+print(max(corner_noreturn))
 print("HILLCLIMBER")
 print(mean_hillclimber)
+print(max(hill_climber))
 print("GREEDY")
 print(mean_greedy)
+print(max(greedy_alg))
+
+visual_solution(NZ_Holland, firstsol_noreturn(NZ_Holland))
+
+###################################################################
+########## HOLLAND
+
+n_random = []
+n_corner = []
+n_random_noreturn = []
+n_corner_noreturn = []
+n_hill_climber = []
+n_greedy_alg = []
+
+for i in range(3):
+    n_random.append(firstsol(Nederland).score())
+    n_corner.append(cornerstart(Nederland).score())
+    n_random_noreturn.append(firstsol_noreturn(Nederland).score())
+    n_corner_noreturn.append(cornerstart_noreturn(Nederland).score())
+    n_hill_climber.append(hillclimber(Nederland, firstsol, random_trajectory_noreturns).score())
+    n_greedy_alg.append(greedy(Nederland).score())
+
+mean_n_random = sum(n_random)/len(n_random)
+mean_n_corner = sum(n_corner)/len(n_corner)
+mean_n_random_noreturn = sum(n_random_noreturn)/len(n_random_noreturn)
+mean_n_corner_noreturn = sum(n_corner_noreturn)/len(n_corner_noreturn)
+mean_n_hillclimber = sum(n_hill_climber)/len(n_hill_climber)
+mean_n_greedy = sum(n_greedy_alg)/len(n_greedy_alg)
+
+lijst = [mean_n_random, mean_n_random_noreturn, mean_n_corner, mean_n_corner_noreturn]
+print(lijst)
+
+
+print("FIRSTSOL:")
+print(mean_n_random)
+print(max(n_random))
+print("UITHOEKSOL:")
+print(mean_n_corner)
+print(max(n_corner))
+print("FIRSTSOL NO RETURN:")
+print(mean_n_random_noreturn)
+print(max(n_random_noreturn))
+print("UITHOEKSOL NO RETURN:")
+print(mean_n_corner_noreturn)
+print(max(n_corner_noreturn))
+print("HILLCLIMBER")
+print(mean_n_hillclimber)
+print(max(n_hill_climber))
+print("GREEDY")
+print(mean_n_greedy)
+print(max(n_greedy_alg))
+
+visual_solution(Nederland, firstsol_noreturn(Nederland))
--- visualisation/visual.py
+++ visualisation/visual.py
@@ -1,29 +1,92 @@
 # In dit document worden de stations en de uiteindelijke optimale
 # route gevisualiseerd dmv van een scatterplot
 
-import matplotlib.mlab as mlab
+import classes.classes
 import matplotlib.pyplot as plt
+from algorithms.firstsol import firstsol
 
 
-# # laad de verschillende stations
-# def visualization(?):
-#     """ Visualiseerd de stations. """
-#     G = nx.Graph()
-#
-#     # voeg alle stations van de classes toe
-#     G.add_node()
-#
-#     # maak lijnen voor sations met sporen ertussen
-#     G.add_path()
-#
-#     #als node kritiek is geef andere kleur dan anderen
-#
-#     # teken de plot en sla afbeelding op
-#     nx.draw(G)
-#     plt.savefig(path.png)
+def visual_solution(data, solution):
+    "Function that visualizes the solution."
 
-def histogram(list):
-    num = len(list)
-    plt.plot(list)
-    plt.ylabel("score")
-    return plt.show()
+    # get stations and coordinates
+    stations = data.names
+    x = data.x
+    y = data.y
+
+    # loop through trains
+    for train in solution.trains:
+        x_cor = []
+        y_cor = []
+
+        past_stations = train.past_stations
+
+        # append x and y coordinates for past stations
+        for station in past_stations:
+            index = stations.index(station)
+            x_cor.append(x[index])
+            y_cor.append(y[index])
+
+        # plot trajectories
+        plt.plot(y_cor, x_cor)
+
+    # plot all trajectories in one plot
+    plt.title('Visualisation Solution')
+    plt.xlabel('y')
+    plt.ylabel('x')
+    plt.show()
+
+
+def visual(data, title):
+    stations = data.names
+    x = data.x
+    y = data.y
+    connections = data.connections
+    critical_connections = data.critical_connections
+    critical_stations = data.critical_stations
+
+    x_cor_s = []
+    y_cor_s = []
+    x_cor_cs = []
+    y_cor_cs = []
+
+    # plot all connections
+    for key, values in connections.items():
+        for value in values:
+            index = stations.index(key)
+            x_cor = x[index]
+            y_cor = y[index]
+
+            index2 = stations.index(value)
+            x_cor2 = x[index2]
+            y_cor2 = y[index2]
+
+            plt.plot([y_cor, y_cor2], [x_cor, x_cor2], "blue")
+
+    # plot critical connections
+    for a, b in critical_connections:
+        index = stations.index(a)
+        x_cor = x[index]
+        y_cor = y[index]
+
+        index2 = stations.index(b)
+        x_cor2 = x[index2]
+        y_cor2 = y[index2]
+
+        plt.plot([y_cor, y_cor2], [x_cor, x_cor2], "red")
+
+    #plot all stations
+    plt.plot(y, x, "bo")
+
+    # plot critical stations
+    for station in critical_stations:
+        index = stations.index(station)
+        x_cor_cs.append(x[index])
+        y_cor_cs.append(y[index])
+
+    plt.plot(y_cor_cs, x_cor_cs, "ro")
+
+    plt.title(title)
+    plt.xlabel("y")
+    plt.ylabel("x")
+    plt.show()

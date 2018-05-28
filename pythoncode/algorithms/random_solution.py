ff976498d94397b6fb9919be904625dc6583d6b2
Kiki van Rongen <Kiki@wcw-stud-145-109-45-63.wireless.uva.nl>
2018-05-28 13:44:21 +0200

    new score function for all critical connections question

--- pythoncode/algorithms/random_solution.py
+++ pythoncode/algorithms/random_solution.py
@@ -1,21 +1,12 @@
 import __init__ as im
 
 def random_solution(data, max_t, max_min, corner = None, returns = None):
-<<<<<<< HEAD
     """ This function gives a random solution given the data, maximum amount of
     trains, maximum amount of minutes, whether to start in a corner or not and
     whether to return to the previous station or not. The stations are choosen
     with the builtin function random. In this function the function random_
     trajectory is called to determine one single trajectory. All these trajectories
     form the whole solution.
-=======
-    """ This function returns a random solution given the data, maximum amount of
-    trains, maximum amount of minutes, start in a corner station or not and
-    allowance of return to previous station. The stations are chosen with the
-    built-in function random. In this function the function random_trajectory is
-    called to determine one single trajectory. All these trajectories together
-    form the entire solution.
->>>>>>> eb94c9b982038815c654ebedf80149c5ca51060f
     """
 
     # initalize variables
